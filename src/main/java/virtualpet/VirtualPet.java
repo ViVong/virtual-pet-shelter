36d06ec09e185ef29fde61636f87c324530a493c
Vi Vong <veixrath@gmail.com>
2018-09-11 13:06:06 -0400

    Added addition functions and changed values for balance

--- src/main/java/virtualpet/VirtualPet.java
+++ src/main/java/virtualpet/VirtualPet.java
@@ -3,14 +3,13 @@ package virtualpet;
 public class VirtualPet {
 
 //Name, Type of Animal, Hunger, Thirst, Disposition, Energy
-//Let (Name) Decide, Energy Determines Rebellion, Abandon Pet.
 
 	//Initializing Public & Private Variables
 	public String name = "<Default>";
 	public String animal = "<Animal>";
-	private int hunger = 20;
-	private int thirst = 20;
-	private int dispo = 25;
+	private int hunger = 25;
+	private int thirst = 25;
+	private int dispo = 20;
 	private int energy = 20;
 	
 	//Constructor with & without input
@@ -22,10 +21,11 @@ public class VirtualPet {
 		animal = Pet;
 	}
 	
-	//Decreases hunger & Outputs verbally hunger
+	//Decreases hunger & Outputs hunger or numerically
 	public void feedPet(int i) {
 		hunger -= i;
 		dispo += (i/2);
+		thirst += 5;
 		energy -= 5;
 	}
 	
@@ -43,10 +43,14 @@ public class VirtualPet {
 			return "Full";
 		}
 	}
+	
+	public int getHungerNum() {
+		return hunger;
+	}
 
-	//Decreases thirst by 5 & Verbally outputs thirst
+	//Decreases thirst + energy & Verbally/numerically outputs thirst
 	public void giveWater() {
-		thirst -= 10;
+		thirst -= 15;
 		energy -= 5;
 	}
 	
@@ -64,22 +68,32 @@ public class VirtualPet {
 			return "Hydrated";
 		}
 	}
+	
+	public int getThirstNum() {
+		return thirst;
+	}
+	
+	//Determines the lowest value changes it
 	public String selfServe() {
-		dispo -= 10;
+		dispo -= 5;
 		if ((hunger > (50 - energy)) || (thirst > (50 - energy))){
 			if (hunger > thirst) {
-				feedPet(5);
+				feedPet(15);
 				return "hungry";
 			}
 			else {
+				giveWater();
 				giveWater();
 				return "thirsty";
 			}
 		}
 		else {
+			energy += 10;
 			return "tired";
 		}
 	}
+	
+	//Verbally or numerically outputs disposition.
 	public String getDispo() {
 		if (dispo > 0 && dispo < 11) { // 1 to 10
 			return "Angry";
@@ -95,38 +109,52 @@ public class VirtualPet {
 		}
 	}
 	
+	public int getDispoNum() {
+		return dispo;
+	}
+	
+	//Increases energy & returns energy value
 	public void takeNap(int i) {
+		hunger -= 3;
+		thirst -= 5;
 		for (int x = i; x!= 0; x--) {
-			energy += 5;
+			energy += 10;
 			tick();
 		}
 	}
 	
 	public String getEnergy() {
-	if (energy < 6) { // 5 or less
-		return "Tired";
-	}
-	else if (energy > 5 && energy < 40) { // 11 to 39
-		return "Active";
-	}
-	else { //40 to 50
-		return "Hyper";
+		if (energy > 0 && energy < 6) { // 5 or less
+			return "Tired";
+		}
+		else if (energy > 5 && energy < 40) { // 11 to 39
+			return "Active";
+		}
+		else if (energy > 39) { //40 to 50
+			return "Full of Energy";
+		}
+		else {
+			return "Exhausted";
+		}
 	}
 	
-}
+	public int getEnergyNum() {
+		return energy;
+	}
 	
+	//tick method
 	public void tick() {
 		if (hunger > 0) {
 			hunger += 3;
 		}
+		else {
+			hunger = 0;
+		}
 		if (thirst > 0) {
 			thirst += 5;
 		}
-		if (energy > 50) {
-			energy = 50;
-		}
-		if (energy < 0) {
-			energy = 0;
+		else {
+			thirst = 0;
 		}
 	}
 }
--- src/main/java/virtualpet/VirtualPetApp.java
+++ src/main/java/virtualpet/VirtualPetApp.java
@@ -1,5 +1,6 @@
 package virtualpet;
 
+import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
@@ -8,25 +9,46 @@ public class VirtualPetApp {
 	public static void main(String[] args) {
 		int choice;
 		VirtualPet dog = new VirtualPet();
+		Random random = new Random();
 		Scanner input = new Scanner(System.in);
-		System.out.println("What kind of pet do you want?");
-		dog.animal = input.next();
-		System.out.println("What is your pet's name?");
+		
+		System.out.println("Welcome to Virtual Pet.\nWould you like to adopt a new pet? Y/N");
+		if ( !input.next().toLowerCase().equals("y")) {
+			System.exit(0);
+		}
+		System.out.println("How to play:\nTry to keep your pet happy and have its needs met.\nYour pet will need energy to eat or drink.\nLetting the pet decide will lower dispostion but decreased other values but more.\nIf hunger/thirst get too high (50) or dispostion gets too low (0) it's Game Over.\nPlease type anything to continue.");
+		input.next();
+		
+		System.out.println("What will you name your pet?");
 		dog.name = input.next();
+		System.out.println("What kind of animal is your pet?");
+		dog.animal = input.next();
+		
+		//Game loop will run as long as hunger/thirst/disposition does not equal 0
 		while( !dog.getHunger().equals("Dead") && !dog.getThirst().equals("Dead") && !dog.getDispo().equals("Ran Away")) {
 			choice = 0;
-			System.out.printf("\n%s the %s\nHunger: %s\nThirst: %s\nDisposition: %s\nEnergy: %s\n\n", dog.name, dog.animal, dog.getHunger(), dog.getThirst(), dog.getDispo(),dog.getEnergy());
+			System.out.printf("\n%s the %s\nHunger: %s (%d)\nThirst: %s (%d)\nDisposition: %s (%d)\nEnergy: %s (%d)\n\n", dog.name, dog.animal, dog.getHunger(), dog.getHungerNum(), dog.getThirst(), dog.getThirstNum(), dog.getDispo(), dog.getDispoNum(), dog.getEnergy(), dog.getEnergyNum());
+			
+			//Prompt user what to do and loops if not within parameters.
 			while (choice < 1 || choice > 5) {
 				System.out.println("1. Feed " + dog.name + "\n2. Give Water\n3. Take a nap\n4. Let " + dog.name + " decide.\n5. Quit\nWhat do you want to do?");
 				choice = Integer.parseInt(input.next());
 				if (choice < 1 || choice > 5) {
 					System.out.println("Invalid choice: Please try again.");
 				}
-				if (dog.getEnergy().equals("Tired") && choice != 3) {
-					System.out.println(dog.name + " is too tired and needs a nap.");
+				//If energy is 0 then you are forced to loop until you take a nap.
+				if (dog.getEnergy().equals("Exhausted") && choice != 3) {
+					System.out.println(dog.name + " is exhausted and needs to rest.");
 					choice = 0;
+					//Pauses program for 2 seconds
+					try {
+						TimeUnit.SECONDS.sleep(2);
+					} catch (InterruptedException e) {
+						Thread.currentThread().interrupt();			
+					}
 				}
 			}
+			//Prompts users to pick between several food options.
 			if (choice == 1) {
 				choice = 0;
 				while (choice < 1 || choice > 3) {
@@ -36,11 +58,19 @@ public class VirtualPetApp {
 						System.out.println("Invalid choice: Please try again.");
 					}
 				}
-					System.out.println("You feed " + dog.name + " some food.");
-					dog.feedPet(choice*3);
+				//Uses a random integer to determine which food the pet like most today.
+				if (random.nextInt(3)+1 == choice){
+					System.out.println( dog.name + " likes your choice of food today and eats happily.");
+					dog.feedPet(12);
+				}
+				else {
+					System.out.println( dog.name + " is neutral about the food you chose today.");
+					dog.feedPet(6);
+				}
 			}
+			//Calls different methods based on choice made earlier.
 			else if (choice == 2) {
-				System.out.println("You give " + dog.name + "water to drink.");
+				System.out.println("You give " + dog.name + " water to drink.");
 				dog.giveWater();
 			}
 			else if (choice == 3) {
@@ -49,26 +79,28 @@ public class VirtualPetApp {
 				dog.takeNap(choice);
 			}
 			else if (choice == 4) {
-				dog.selfServe();
+				System.out.println( dog.name + " decided they were " + dog.selfServe());
 			}
 			else {
 				System.exit(0);
 			}
 			dog.tick();
+			//Pauses program for 2 seconds
 			try {
 				TimeUnit.SECONDS.sleep(2);
 			} catch (InterruptedException e) {
 				Thread.currentThread().interrupt();			
-				}
+			}
 		}
+		//Outputs GAME OVER and tells which variable caused the loop to end.
 		if (dog.getHunger().equals("Dead")) {
-			System.out.println("Your " + dog.animal + " died from because you forgot to feed it.");
+			System.out.println("\nYour " + dog.animal + " died from because you forgot to feed it.\nGAME OVER");
 		}
 		if (dog.getThirst().equals("Dead")) {
-			System.out.println("Your " + dog.animal + "died from the opposite of drowning.");
+			System.out.println("\nYour " + dog.animal + " died from the opposite of drowning.\nGAME OVER");
 		}
 		if (dog.getDispo().equals("Ran Away")) {
-			System.out.println("Your " + dog.animal + " ran away because you are a terrible owner.");
+			System.out.println("\nYour " + dog.animal + " ran away because it felt you didn't care enough.\nGAME OVER");
 		}
 
 	}
