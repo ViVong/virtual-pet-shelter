b7100b8f00b45f41823a19d9f2413f9cac0aa0ae
Vi Vong <veixrath@gmail.com>
2018-09-17 16:37:02 -0400

    Added method to virtual pet shelter

--- src/main/java/virtualpetshelter/VirtualPetShelter.java
+++ src/main/java/virtualpetshelter/VirtualPetShelter.java
@@ -3,5 +3,35 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class VirtualPetShelter {
-	
+
+	HashMap<String, VirtualPet> shelter = new HashMap<String, VirtualPet>();
+	public void allPets() {
+		
+	}
+	public VirtualPet onePet(String name) {
+		return shelter.get(name);
+	}
+	public void feedPets() {
+		
+	}
+	public void waterPets() {
+		
+	}
+	public void playWith(String name) {
+		shelter.get(name).playWith();
+	}
+	public void adopt(String name) {
+		shelter.remove(name);
+	}
+	public void admit(String name, String description) {
+		VirtualPet newPet = new VirtualPet(name, description);
+		shelter.put(newPet.name, newPet);
+	}
+	public void admit(String name, String description, int food, int water, int mood) {
+		VirtualPet newPet = new VirtualPet(name, description, food, water, mood);
+		shelter.put(newPet.name, newPet);
+	}
+	public void tick() {
+		
+	}
 }
